e3d5ae31a0b312c8d5ca3594a86e86bf3945ffa3
TimotheeMonceaux <timothee.monceaux@gmail.com>
2014-05-18 17:24:02 +0200

    Modifié UTProfiler.h pour le faire correspondre a l'UML.

--- UTProfiler/UTProfiler.h
+++ UTProfiler/UTProfiler.h
@@ -43,25 +43,66 @@ enum Categorie {
 
 class UV {
     string code;
-    string intitule;
+    string titre;
     string responsable;
-    unsigned int *nbCredits;
-    Categorie *categorie;
+    unsigned int nbCredits;
+    Categorie categorie;
     bool printemps;
     bool automne;
     //unsigned int nbCategories;
+   public :
+    UV();//à voir
+    string getCode() const;
+    string getTitre() const;
+    string getResponsable() const;
+    Categorie getCategorie() const;
+    unsigned int getNbCredits() const;
+    bool getPrintemps() const;
+    bool getAutomne() const;
 };
 
 class Inscription {
     string uv;
     Resultat resultat;
+   public :
+    Inscription();
+    string getUV() const;
+    Resultat getResultat() const;
+    void setResultat(Resultat r);
+};
+
+class FormationExterieure { //à revoir si stage => formations exterieures
+    string nom;
+    string lieu;
+    unsigned int creditsCS;
+    unsigned int creditsTM;
+    unsigned int creditsTSH;
+    unsigned int creditsSP;
+   public :
+    FormationExterieure();
+    ~FormationExterieure();
+    string getNom() const;
+    string getLieu() const;
+    unsigned int getCreditsCS() const;
+    unsigned int getCreditsTM() const;
+    unsigned int getCreditsTSH() const;
+    unsigned int getCreditsSP() const;
 };
 
 class Semestre {
     unsigned int annee;
     Saison saison;
-    Inscription *inscriptions;
     string commentaire;
+    Inscription *inscriptions;
+    bool ext;
+    FormationExterieure *formationsExt;
+   public :
+    Semestre();
+    ~Semestre();
+    unsigned int getAnnee() const;
+    Saison getSaison() const;
+    string getCommentaire() const;
+    bool getExt() const;
 };
 
 class Dossier {
@@ -71,9 +112,24 @@ class Dossier {
     string conseiller;
     bool validationAEU;
     Semestre *semestres;
+   public :
+    Dossier();
+    ~Dossier();
+    string getLogin() const;
+    string getNom() const;
+    string getPrenom() const;
+    string getConseiller() const;
+    bool getValidationAEU() const;
+};
+
+class Formation { //à retravailler
 };
 
 class UVManager {
+    //Singleton
+};
+
+class FormationManager {
     //Singleton
 };
 
