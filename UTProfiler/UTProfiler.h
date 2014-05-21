5a72cc1cd684a3c54871f2ea5a5328556c6ffd0f
TimotheeMonceaux <timothee.monceaux@gmail.com>
2014-05-21 17:41:14 +0200

    Fin de la plus grosse partie de la conception. UML et code correspondent. Il reste plus qu'a retravailler Formation ensemble et à mettre en place les différents manager

--- UTProfiler/UTProfiler.h
+++ UTProfiler/UTProfiler.h
@@ -41,6 +41,10 @@ enum Categorie {
     SP
 };
 
+class UTProfilerException : public std::exception {
+
+};
+
 class UV {
     string code;
     string titre;
@@ -49,16 +53,31 @@ class UV {
     Categorie categorie;
     bool printemps;
     bool automne;
-    //unsigned int nbCategories;
    public :
-    UV();//à voir
-    string getCode() const;
-    string getTitre() const;
-    string getResponsable() const;
-    Categorie getCategorie() const;
-    unsigned int getNbCredits() const;
-    bool getPrintemps() const;
-    bool getAutomne() const;
+    UV();//à voir : c'est à UVManager de gérer leurs cycles de vie
+    string getCode() const {return code;}
+    string getTitre() const {return titre;}
+    string getResponsable() const {return responsable;}
+    Categorie getCategorie() const {return categorie;}
+    unsigned int getNbCredits() const {return nbCredits;}
+    bool getPrintemps() const {return printemps;}
+    bool getAutomne() const {return automne;}
+    void setCode(const string& s) {code=s;}
+    void setTitre(const string& s) {titre=s;}
+    void setNbCredits (unsigned int i) {nbCredits=i;}
+    void setCategorie (Categorie& c) {categorie=c;}
+    void setPrintemps (bool b) {printemps=b;}
+    void setAutomne (bool b) {automne=b;}
+/* Proposition concernant les UVs pouvant avoir deux catégories :
+ * struct Credits {
+ *    Categorie categorie;
+ *    unsigned int nbCredits;
+ * }
+ * Credits *credits;
+ *
+ * credits fonctionnerai comme un tableau : la plupart des UVs ferai genre : credits = new Credits;
+ * mais certaines pourrai faire : credits = new Credits[2];
+ * Un iterateur nous permettrai de gérer les cas ou il peut y en avoir plusieurs */
 };
 
 class Inscription {
@@ -66,12 +85,13 @@ class Inscription {
     Resultat resultat;
    public :
     Inscription();
-    string getUV() const;
-    Resultat getResultat() const;
-    void setResultat(Resultat r);
+    string getUV() const {return uv;}
+    Resultat getResultat() const {return resultat;}
+    void setUv (const string& s) {uv=s;}
+    void setResultat(Resultat& r) {resultat=r;}
 };
 
-class FormationExterieure { //à revoir si stage => formations exterieures
+class FormationExterieure {
     string nom;
     string lieu;
     unsigned int creditsCS;
@@ -79,14 +99,21 @@ class FormationExterieure { //à revoir si stage => formations exterieures
     unsigned int creditsTSH;
     unsigned int creditsSP;
    public :
-    FormationExterieure();
-    ~FormationExterieure();
-    string getNom() const;
-    string getLieu() const;
-    unsigned int getCreditsCS() const;
-    unsigned int getCreditsTM() const;
-    unsigned int getCreditsTSH() const;
-    unsigned int getCreditsSP() const;
+    FormationExterieure (const string& n, const string& l, unsigned int cs, unsigned int tm, unsigned int tsh, unsigned int sp) : nom(n), lieu(l), creditsCS(cs), creditsTM(tm), creditsTSH(tsh), creditsSP(sp) {}
+    FormationExterieure (FormationExterieure& other) : nom(other.getNom()), lieu(other.getLieu()), creditsCS(other.getCreditsCS()), creditsTM(other.getCreditsTM()), creditsTSH(other.getCreditsTSH()), creditsSP(other.getCreditsSP()) {}
+    ~FormationExterieure() {}
+    string getNom() const {return nom;}
+    string getLieu() const {return lieu;}
+    unsigned int getCreditsCS() const {return creditsCS;}
+    unsigned int getCreditsTM() const {return creditsTM;}
+    unsigned int getCreditsTSH() const {return creditsTSH;}
+    unsigned int getCreditsSP() const {return creditsSP;}
+    void setNom (const string& s) {nom=s;}
+    void setLieu (const string& s) {lieu=s;}
+    void setCreditsCS (unsigned int i) {creditsCS=i;}
+    void setCreditsTM (unsigned int i) {creditsTM=i;}
+    void setCreditsTSH (unsigned int i) {creditsTSH=i;}
+    void setCreditsSP (unsigned int i) {creditsSP=i;}
 };
 
 class Semestre {
@@ -95,14 +122,30 @@ class Semestre {
     string commentaire;
     Inscription *inscriptions;
     bool ext;
-    FormationExterieure *formationsExt;
+    FormationExterieure *formationExt;
    public :
-    Semestre();
+    Semestre(unsigned int a, Saison s, string c="");// ???
     ~Semestre();
-    unsigned int getAnnee() const;
-    Saison getSaison() const;
-    string getCommentaire() const;
-    bool getExt() const;
+    unsigned int getAnnee() const {return annee;}
+    Saison getSaison() const {return saison;}
+    string getCommentaire() const {return commentaire;}
+    bool getExt() const {return ext;}
+    FormationExterieure* getFormationExterieure() const {return formationExt;}
+    //iterator pour naviguer entre les inscriptions
+    void setAnnee (unsigned int i) {annee=i;}
+    void setSaison (Saison& s) {saison=s;}
+    void setCommentaire (const string& s) {commentaire=s;}
+    void setFormationExterieure (FormationExterieure* fe) {formationExt=fe;}
+    /*Explication du fonctionnement de cette classe :
+     * Un Semestre (pour un étudiant) est soit un semestre à l'UTC, au quel cas l'étudiant suit un certain nombre d'UV,
+     * alors répertoriées dans le tableau inscriptions. le pointeur formationExt
+     * Remarque : un stage est un semestre contenant une inscription à une unique UV : TN09/TN10.
+     *
+     * S'il a suivi une formation exterieure (équivalence ou Erasmus), alors on lève le flag ext, le pointeur inscriptions
+     * doit alors pointer vers NULL et aucun iterateur ne doit exister. La formation exterieure est alors décrite
+     * par le pointeur prévu à cet effet.
+     *
+     * Tout ceci doit être géré dans le constructeur. */
 };
 
 class Dossier {
@@ -115,22 +158,37 @@ class Dossier {
    public :
     Dossier();
     ~Dossier();
-    string getLogin() const;
-    string getNom() const;
-    string getPrenom() const;
-    string getConseiller() const;
-    bool getValidationAEU() const;
+    string getLogin() const {return login;}
+    string getNom() const {return nom;}
+    string getPrenom() const {return prenom;}
+    string getConseiller() const {return conseiller;}
+    bool getValidationAEU() const {return validationAEU;}
+    //iterator pour naviguer entre les semestres
+    void setLogin (const string& s) {login=s;}
+    void setNom (const string& s) {nom=s;}
+    void setPrenom (const string& s) {prenom=s;}
+    void setConseiller (const string& s) {conseiller=s;}
+    void setValidationAEU (bool b) {validationAEU=b;}
 };
 
 class Formation { //à retravailler
 };
 
-class UVManager {
+class Manager {
     //Singleton
+    //Classe Abstraite
 };
 
-class FormationManager {
-    //Singleton
+class UVManager : public Manager {
+    //On n'a plus à se soucier du singleton : héritage de Manager
+};
+
+class FormationManager : public Manager {
+    //On n'a plus à se soucier du singleton : héritage de Manager
+};
+
+class DossierManager : public Manager {
+    //On n'a plus à se soucier du singleton : héritage de Manager
 };
 
 #endif // UTPROFILER_H
