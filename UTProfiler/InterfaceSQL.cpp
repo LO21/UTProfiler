c7901157709f8cf7ed659e4767cda2f172350e44
TimotheeMonceaux <timothee.monceaux@gmail.com>
2014-06-04 21:27:14 +0200

    InterfaceSQL fonctionne

--- UTProfiler/InterfaceSQL.cpp
+++ UTProfiler/InterfaceSQL.cpp
@@ -13,7 +13,7 @@ void InterfaceSQL::libererInstance() {
 
 InterfaceSQL::InterfaceSQL() {
     db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("DataBase");
+    db.setDatabaseName("UTProfiler.db");
     if (!db.open()) {QMessageBox::critical(0,"Erreur de connexion à  la base de données",db.lastError().text());}
     query = new QSqlQuery(db);
 }
@@ -26,7 +26,18 @@ bool InterfaceSQL::load() {
     if (!f.open(QIODevice::ReadOnly)) {throw UTProfilerException(QString::fromStdString("Erreur : Le fichier '"+chemin.toStdString()+"' ne s'ouvre' pas.")); return false;}
     QTextStream lecture(&f);
     QString s=lecture.readAll();
-    execQuery(s);
+    unsigned int i=0;
+    QString q = "";
+    while (s[i]!='\0') {
+        if (s[i]==';') {
+            q.append(s[i]);
+            execQuery(q);
+            q.clear();
+            i+=3;
+        }
+        q.append(s[i]);
+        ++i;
+    }
     return true;
 }
 
@@ -36,12 +47,22 @@ bool InterfaceSQL::load(const QString &chemin) {
     if (!f.open(QIODevice::ReadOnly)) {throw UTProfilerException(QString::fromStdString("Erreur : Le fichier '"+chemin.toStdString()+"' ne s'ouvre' pas.")); return false;}
     QTextStream lecture(&f);
     QString s=lecture.readAll();
-    execQuery(s);
+    unsigned int i=0;
+    QString q = "";
+    while (s[i]!='\0') {
+        if (s[i]==';') {
+            q.append(s[i]);
+            execQuery(q);
+            q.clear();
+            i+=3;
+        }
+        q.append(s[i]);
+        ++i;
+    }
     return true;
 }
 
 QSqlQuery& InterfaceSQL::execQuery(const QString &q) {
-    db.open();
     if (!query->exec(q)) {throw UTProfilerException(QString::fromStdString("Erreur : La requête :\n")+q+QString::fromStdString("\n n'a pas fonctionné.\nDernière Erreur : ")+query->lastError().text());}
     return *query;
 }
---
+++ build-UTProfiler-Desktop-Debug/requete.sql
@@ -0,0 +1,90 @@
+CREATE TABLE IF NOT EXISTS UV (
+	code VARCHAR(5) PRIMARY KEY,
+	titre VARCHAR(100) NOT NULL,
+	responsable VARCHAR(30) NOT NULL,
+	creditsCS INTEGER(2),
+	creditsTM INTEGER(2),
+	creditsTSH INTEGER(2),
+	creditsSP INTEGER(2),
+	printemps BOOL,
+	automne BOOL);
+
+CREATE TABLE IF NOT EXISTS Branche (
+	b VARCHAR(10));
+
+INSERT INTO Branche VALUES ('TC');
+
+INSERT INTO Branche VALUES ('HUTECH');
+
+INSERT INTO Branche VALUES ('GB');
+
+INSERT INTO Branche VALUES ('GI');
+
+INSERT INTO Branche VALUES ('GM');
+
+INSERT INTO Branche VALUES ('GP');
+
+INSERT INTO Branche VALUES ('GSM');
+
+INSERT INTO Branche VALUES ('GSU');
+
+CREATE TABLE IF NOT EXISTS Dossier (
+	login CHAR(8) PRIMARY KEY,
+	nom VARCHAR(25) NOT NULL,
+	prenom VARCHAR(25) NOT NULL,
+	conseiller VARCHAR(25) NOT NULL,
+	validationAEU BOOL,
+	branche VARCHAR(10) REFERENCES Branche(b),
+	mineur VARCHAR(10));
+
+CREATE TABLE IF NOT EXISTS Saison (
+	s CHAR(1));
+
+INSERT INTO Saison VALUES ('A');
+
+INSERT INTO Saison VALUES ('P');
+
+CREATE TABLE IF NOT EXISTS Semestre (
+	id INTEGER(10) PRIMARY KEY,
+	saison CHAR(1) REFERENCES Saison(s),
+	annee INTEGER(4),
+	dossier CHAR(8) REFERENCES Dossier(login),
+	commentaire VARCHAR(100),
+	ext BOOL);
+	
+CREATE TABLE Resultat (
+	r VARCHAR(3));
+
+INSERT INTO Resultat VALUES ('A');
+
+INSERT INTO Resultat VALUES ('B');
+
+INSERT INTO Resultat VALUES ('C');
+
+INSERT INTO Resultat VALUES ('D');
+
+INSERT INTO Resultat VALUES ('E');
+
+INSERT INTO Resultat VALUES ('FX');
+
+INSERT INTO Resultat VALUES ('F');
+
+INSERT INTO Resultat VALUES ('EC');
+
+INSERT INTO Resultat VALUES ('RES');
+
+CREATE TABLE IF NOT EXISTS Inscription (
+	uv VARCHAR(5) REFERENCES UV(code),
+	semestre INTEGER(10) REFERENCES Semestre(id),
+	resultat VARCHAR(3) REFERENCES Resultat(r),
+	PRIMARY KEY (uv, semestre) );
+
+CREATE TABLE IF NOT EXISTS FormationExt (
+	nom VARCHAR(50),
+	semestre INTEGER(10) REFERENCES Semestre(id),
+	lieu VARCHAR(50) NOT NULL,
+	creditsCS INTEGER(2),
+	creditsTM INTEGER(2),
+	creditsTSH INTEGER(2),
+	creditsSP INTEGER(2),
+	PRIMARY KEY (semestre, nom) );
