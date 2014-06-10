d780c22466afc04f806b6f20e256628e03c34c8a
rgabi92 <gabrielle.rit@gmail.com>
2014-06-10 09:40:49 +0200

    Vérification de la casse pour formation ext

--- UTProfiler/main.cpp
+++ UTProfiler/main.cpp
@@ -6,7 +6,7 @@ int main (int argc, char* argv[]) {
     QApplication app(argc, argv);
     DossierWindow *fen = new DossierWindow;
     InterfaceSQL *sql = InterfaceSQL::getInstance();
-    //sql->load("G:\\Documents\\LO21\\UTProfiler\\formation.sql");
+    //sql->load("/Users/Gabou/Dropbox/Cours/GI02/LO21/UTProfiler/formation.sql");
     fen->show();
     return app.exec();
 
